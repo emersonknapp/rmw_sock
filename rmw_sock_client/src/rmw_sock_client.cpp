fd4cb85624d6b5d498b8ae4fc349ecb18c7c36e3
Emerson Knapp <emerson.b.knapp@gmail.com>
2019-11-15 16:55:58 -0800

    Server and client can talk to each other
    Signed-off-by: Emerson Knapp <emerson.b.knapp@gmail.com>

--- rmw_sock_client/src/rmw_sock_client.cpp
+++ rmw_sock_client/src/rmw_sock_client.cpp
@@ -11,14 +11,43 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#include "Poco/Net/HTTPRequest.h"
+#include "Poco/Net/HTTPResponse.h"
+#include "Poco/Net/HTTPMessage.h"
+#include "Poco/Net/WebSocket.h"
+#include "Poco/Net/HTTPClientSession.h"
+#include <iostream>
 
-#include <cstdio>
+using Poco::Net::HTTPClientSession;
+using Poco::Net::HTTPRequest;
+using Poco::Net::HTTPResponse;
+using Poco::Net::HTTPMessage;
+using Poco::Net::WebSocket;
 
-int main(int argc, char ** argv)
+
+int main()
 {
-  (void) argc;
-  (void) argv;
+    HTTPClientSession cs("localhost", 8080);
+    HTTPRequest request(HTTPRequest::HTTP_GET, "/?encoding=text",HTTPMessage::HTTP_1_1);
+    request.set("origin", "localhost");
+    HTTPResponse response;
 
-  printf("hello world rmw_sock_client package\n");
-  return 0;
+    try {
+        WebSocket* m_psock = new WebSocket(cs, request, response);
+        char const *testStr="Hello echo websocket!";
+        char receiveBuff[256];
+
+        int len=m_psock->sendFrame(testStr,strlen(testStr),WebSocket::FRAME_TEXT);
+        std::cout << "Sent bytes " << len << std::endl;
+        int flags=0;
+
+        int rlen=m_psock->receiveFrame(receiveBuff,256,flags);
+        std::cout << "Received bytes " << rlen << std::endl;
+        std::cout << receiveBuff << std::endl;
+
+        m_psock->close();
+    } catch (std::exception &e) {
+        std::cout << "Exception " << e.what();
+    }
+
 }
